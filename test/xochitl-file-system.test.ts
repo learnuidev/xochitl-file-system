4e3418f0580f5bc4ec3f45ffd116a3f0a3aab7a0
learnuidev <learnuidev@gmail.com>
2025-04-02 00:32:12 -0400

    Initial commit

---
+++ test/xochitl-file-system.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XochitlFileSystem from '../lib/xochitl-file-system-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xochitl-file-system-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XochitlFileSystem.XochitlFileSystemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
